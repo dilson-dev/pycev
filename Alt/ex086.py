e7b85061697ef38b46c6a92b7a6e326da621cc15
Emanoel Delfino <emanoelsilva1111@gmail.com>
2020-05-19 15:21:31 -0300

    Delete Alt folder with its files

--- Alt/ex086.py
+++
@@ -1,6 +0,0 @@
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for linha in range(3):
-    for coluna in range(3):
-        matriz[linha][coluna] = int(input(f'Posição {[linha, coluna]}: '))
-# print(v for v in lista for lista in matriz)
-[print(f'{" | ".join(str(v) for v in lista)}') for lista in matriz]
